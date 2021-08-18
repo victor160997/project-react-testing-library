386a1ddcc3ba7f63198274f91862fef24f757cc7
Vinicius <viniderp@gmail.com>
2021-08-18 19:11:02 -0300

    3rd requirement

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import FavoritePokemons from '../components/FavoritePokemons';
+import pokemons from '../data';
+
+describe('', () => {
+  test('Not found message', () => {
+    render(
+      <MemoryRouter>
+        <FavoritePokemons pokemons={ [] } />
+      </MemoryRouter>,
+    );
+
+    screen.getByText('No favorite pokemon found');
+  });
+
+  test('Render favorite pokemons', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/favorites'] }>
+        <FavoritePokemons pokemons={ pokemons } />
+      </MemoryRouter>,
+    );
+
+    const names = screen.getAllByTestId('pokemon-name');
+
+    expect(names.length).toBe(pokemons.length);
+  });
+});
