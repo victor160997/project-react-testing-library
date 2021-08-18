c829729bc5666a05f4528fc367a2fada6f6dfc6a
Vinicius <viniderp@gmail.com>
2021-08-18 20:14:01 -0300

    4th requirement

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('', () => {
+  test('Heading', () => {
+    render(
+      <NotFound />,
+    );
+
+    const heading = screen.getByRole('heading');
+
+    expect(heading.textContent).toBe('Page requested not found 😭');
+  });
+
+  test('Image', () => {
+    render(
+      <NotFound />,
+    );
+
+    const img = screen.getAllByRole('img')[1];
+
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
