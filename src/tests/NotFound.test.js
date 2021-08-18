6d26a7cf01ed1ef050cf1f3bcbbf55b57892d6fc
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-08-18 16:55:28 -0300

    requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,32 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+    customHistory.push('dhdfh');
+    const have = screen.getByText(/Page requested not found/i);
+    expect(have).toBeInTheDocument();
+  });
+
+  it('Teste se página mostra a imagem', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+    customHistory.push('dhdfh');
+
+    const have = screen.getByAltText(/Pikachu crying/i);
+    expect(have.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
