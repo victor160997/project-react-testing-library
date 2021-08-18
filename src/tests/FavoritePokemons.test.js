399df9b27cda091a9f679cafa103fa2378ce741d
Ygor Lage <75700712+Yg-or@users.noreply.github.com>
2021-08-18 16:46:37 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import App from '../App';
+
+import * as services from '../services/pokedexService';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it('Teste se é exibido na tela a mensagem No favorite pokemon found', () => {
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+    customHistory.push('favorites');
+
+    const noFavorites = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorites).toBeInTheDocument();
+  });
+});
+
+describe('teste 2', () => {
+  it('Teste se é exibido todos os cards de pokémons favoritados.', () => {
+    const id = 10;
+    services.updateFavoritePokemons(id, true);
+
+    const customHistory = createMemoryHistory();
+    render(
+      <Router history={ customHistory }>
+        <App />
+      </Router>,
+    );
+    customHistory.push('favorites');
+
+    const all = screen.getAllByTestId('pokemon-name');
+    expect(all.length).toBe(1);
+  });
+});
